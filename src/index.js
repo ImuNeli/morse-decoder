e728ca725f8efb89d6385fb26f8ec655d30904a3
ImuNeli <nelly.yakubova@gmail.com>
2022-08-10 09:56:51 +0300

    feat: finished

--- src/index.js
+++ src/index.js
@@ -38,8 +38,23 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let result = ''; 
+    let symbol = '';
+    for (i = 0; i < expr.length; i += 10) {
+        symbol = expr.slice(i, i+10);
+
+        if (symbol == '**********') {
+            result += ' ';
+        }
+
+        else {
+            let morz = symbol.replace(/10/g, '.').replace(/11/g, '-').replace(/0/g, '');
+            result += MORSE_TABLE[morz];
+        }
+   }
+
+   return result;
+ }
 
 module.exports = {
     decode
